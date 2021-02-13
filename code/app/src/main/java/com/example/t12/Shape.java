0ed249a7b70197ab4766c1d6fb63b168a97e4cec
Ben Wang <xiaoyu.w.99@gmail.com>
2021-02-13 10:59:15 -0700

    created abstract shape

---
+++ code/app/src/main/java/com/example/t12/Shape.java
@@ -0,0 +1,12 @@
+package com.example.t12;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    public Shape(int x, int y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+}
