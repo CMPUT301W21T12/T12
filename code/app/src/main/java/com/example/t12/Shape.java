a7ec55de2912e77ecd49ff7bc41237f8426f39fe
celina <csheng2@ualberta.ca>
2021-02-14 21:37:41 -0700

    resolved conflicts with main

--- code/app/src/main/java/com/example/t12/Shape.java
+++ code/app/src/main/java/com/example/t12/Shape.java
@@ -3,14 +3,12 @@ package com.example.t12;
 public abstract class Shape {
     private int x;
     private int y;
-    private String colour;
-
-    public Shape(int x, int y, String colour)
     private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
-    public Shape(int x, int y)
+
+    public Shape(int x, int y, String color)
     {
         this.x=x;
         this.y=y;
-        this.colour = "blue";
+        this.color = "blue";
     }
 }
