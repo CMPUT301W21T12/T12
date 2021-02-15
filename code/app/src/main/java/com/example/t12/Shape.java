ca0b5d5dededab5cac6e179a0cc1736dc88e9382
Ben Wang <xiaoyu.w.99@gmail.com>
2021-02-14 20:20:02 -0700

    added color to shape

--- code/app/src/main/java/com/example/t12/Shape.java
+++ code/app/src/main/java/com/example/t12/Shape.java
@@ -3,7 +3,7 @@ package com.example.t12;
 public abstract class Shape {
     private int x;
     private int y;
-
+    String color = "Purple";
     public Shape(int x, int y)
     {
         this.x=x;
