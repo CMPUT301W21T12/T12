f74d0e9c67c5b6cd40c3ce8af41dce8537816b61
celina <csheng2@ualberta.ca>
2021-02-14 17:33:54 -0700

    celina's commit to Shape.java that won't be pushed

--- code/app/src/main/java/com/example/t12/Shape.java
+++ code/app/src/main/java/com/example/t12/Shape.java
@@ -3,10 +3,12 @@ package com.example.t12;
 public abstract class Shape {
     private int x;
     private int y;
+    private String colour;
 
-    public Shape(int x, int y)
+    public Shape(int x, int y, String colour)
     {
         this.x=x;
         this.y=y;
+        this.colour = "blue";
     }
 }
