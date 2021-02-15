254f64cfcfca1e36aa9b96f3a9bc76442a9675d8
Joseph Dimaano <jdimaano@ualberta.ca>
2021-02-14 20:53:20 -0700

    pushed green to string color

--- code/app/src/main/java/com/example/t12/Shape.java
+++ code/app/src/main/java/com/example/t12/Shape.java
@@ -3,11 +3,7 @@ package com.example.t12;
 public abstract class Shape {
     private int x;
     private int y;
-<<<<<<< HEAD
-
-=======
-    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
->>>>>>> f25bb1a3638e5f174ceabaa673f416c8c2cbe845
+    private String color = "green";
     public Shape(int x, int y)
     {
         this.x=x;
